24ccfd9b43aa7be4d82c6c4132cc55e7d4acb725
piyush arora <piyusharorawork@gmail.com>
2024-08-10 19:18:43 +0530

    createBinaryTreeFromArray

---
+++ problem-statements/create-binary-tree-from-array/create-binary-tree-from-array.js
@@ -0,0 +1,44 @@
+import { Queue } from "../implement-queue/queue";
+
+export class TreeNode {
+  constructor(val, left, right) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+export function createBinaryTreeFromArray(nums) {
+  const queue = new Queue();
+
+  if (nums.length === 0 || nums[0] === null) {
+    return queue;
+  }
+
+  const root = new TreeNode(nums[0]);
+  queue.enqueue(root);
+
+  let i = 1;
+
+  for (i = 1; i < nums.length; i++) {
+    const node = queue.dequeue();
+
+    // Update Left Child
+    if (nums[i]) {
+      const leftNode = new TreeNode(nums[i]);
+      node.left = leftNode;
+      queue.enqueue(nums[i]);
+    }
+    i++;
+
+    // Update Right Child
+    if (nums[i]) {
+      const rightNode = new TreeNode(nums[i]);
+      node.right = rightNode;
+      queue.enqueue(nums[i]);
+    }
+    i++;
+  }
+
+  return root;
+}
