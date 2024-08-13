99c5e87c2a515b9a74f7657bdeb60caed2b64c20
piyush arora <piyusharorawork@gmail.com>
2024-08-13 21:58:39 +0530

    linked list cycle

---
+++ problem-statements/linked-lists/linked-list-cycle/linked-list-cycle.js
@@ -0,0 +1,19 @@
+export function hasCycle(head) {
+  if (head === null) {
+    return false;
+  }
+
+  let slow = head;
+  let fast = head.next;
+
+  while (fast !== null && fast.next !== null) {
+    if (slow === fast) {
+      return true;
+    }
+
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return false;
+}
