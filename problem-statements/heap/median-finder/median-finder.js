fa366d54372c25f83e298f694c211fb93df323b5
piyush arora <piyusharorawork@gmail.com>
2024-09-01 05:34:30 +0530

    median finder

---
+++ problem-statements/heap/median-finder/median-finder.js
@@ -0,0 +1,196 @@
+import {
+  MaxHeap,
+  MinHeap,
+  PriorityQueue,
+} from "../../../data-structures/heap/heap";
+
+// Using min and max heap
+export class MedianFinderV1 {
+  constructor() {
+    this.maxHeap = new MaxHeap();
+    this.minHeap = new MinHeap();
+  }
+
+  addNum(num) {
+    if (this.maxHeap.size() === 0) {
+      this.maxHeap.insert(num);
+    }
+
+    // if min heap size is smaller
+    else if (this.minHeap.size() < this.maxHeap.size())
+      this.minHeap.insert(num);
+    // if size are equal
+    else {
+      this.maxHeap.insert(num);
+
+      // balance the heaps
+
+      const val1 = this.minHeap.remove();
+      const val2 = this.maxHeap.remove();
+
+      this.minHeap.insert(val2);
+      this.maxHeap.insert(val1);
+    }
+  }
+
+  findMedian() {
+    if (this.maxHeap.size() > this.minHeap.size()) {
+      return this.maxHeap.peek();
+    }
+
+    const mid1 = this.maxHeap.peek();
+    const mid2 = this.minHeap.peek();
+
+    return (mid1 + mid2) / 2;
+  }
+}
+
+// Using Priority Queue
+export class MedianFinderV2 {
+  constructor() {
+    this.maxPQ = new PriorityQueue({
+      compare: (a, b) => b.priority - a.priority,
+    });
+    this.minPQ = new PriorityQueue({
+      compare: (a, b) => a.priority - b.priority,
+    });
+  }
+
+  addNum(num) {
+    if (this.maxPQ.size() === 0)
+      this.maxPQ.enqueue({ value: num, priority: num });
+    else if (this.minPQ.size() < this.maxPQ.size())
+      this.minPQ.enqueue({ value: num, priority: num });
+    // if sizes are equal
+    else {
+      this.maxPQ.enqueue({ value: num, priority: num });
+
+      // balance the pqs
+      const val1 = this.minPQ.dequeue().value;
+      const val2 = this.maxPQ.dequeue().value;
+
+      this.minPQ.enqueue({ value: val2, priority: val2 });
+      this.maxPQ.enqueue({ value: val1, priority: val1 });
+    }
+  }
+
+  findMedian() {
+    if (this.maxPQ.size() > this.minPQ.size()) return this.maxPQ.front().value;
+
+    const mid1 = this.maxPQ.front().value;
+    const mid2 = this.minPQ.front().value;
+
+    return (mid1 + mid2) / 2;
+  }
+}
+
+// Using Cleaer Priority Queue
+export class MedianFinderV3 {
+  constructor() {
+    this.maxPQ = new PriorityQueue({
+      compare: (a, b) => b - a,
+    });
+    this.minPQ = new PriorityQueue({
+      compare: (a, b) => a - b,
+    });
+  }
+
+  addNum(num) {
+    this.maxPQ.enqueue(num);
+
+    // We need to ensure all elements in the maxPQ <= elements in the minPQ
+    if (
+      this.maxPQ.size() > 0 &&
+      this.minPQ.size() > 0 &&
+      this.maxPQ.front() > this.minPQ.front()
+    ) {
+      const num1 = this.maxPQ.dequeue();
+      const num2 = this.minPQ.dequeue();
+
+      this.maxPQ.enqueue(num2);
+      this.minPQ.enqueue(num1);
+    }
+
+    // We need to ensure that the size of maxPQ cannot excceed minPQ Size +1
+    if (this.maxPQ.size() > this.minPQ.size() + 1) {
+      this.minPQ.enqueue(this.maxPQ.dequeue());
+    } else if (this.minPQ.size() > this.maxPQ.size()) {
+      this.maxPQ.enqueue(this.minPQ.dequeue());
+    }
+  }
+
+  findMedian() {
+    if (this.maxPQ.size() > this.minPQ.size()) return this.maxPQ.front();
+
+    const mid1 = this.maxPQ.front();
+    const mid2 = this.minPQ.front();
+
+    return (mid1 + mid2) / 2;
+  }
+}
+
+// Using array
+export class MedianFinderV4 {
+  constructor() {
+    this.list = [];
+  }
+
+  addNum(num) {
+    this.list.push(num);
+  }
+
+  findMedian() {
+    const N = this.list.length;
+
+    if (N === 1) return this.list[0];
+
+    // ODD
+    if (N % 2 === 1) {
+      const index = Math.floor(N / 2); // 7 => 3
+      return this.list[index];
+    }
+
+    const leftIdx = Math.floor((N - 1) / 2); // 8 => 3,4
+    const rightIdx = leftIdx + 1;
+    return (this.list[leftIdx] + this.list[rightIdx]) / 2;
+  }
+}
+
+export class MedianFinder {
+  constructor() {
+    this.maxPQ = new PriorityQueue({ compare: (a, b) => b - a });
+    this.minPQ = new PriorityQueue({ compare: (a, b) => a - b });
+  }
+
+  addNum(num) {
+    // always add to maxPQ
+    this.maxPQ.enqueue(num);
+
+    // balance the pq such that no elements in max PQ has value higher than minPQ
+    if (
+      this.maxPQ.size() > 0 &&
+      this.minPQ.size() > 0 &&
+      this.maxPQ.front() > this.minPQ.front()
+    ) {
+      const num1 = this.maxPQ.dequeue();
+      const num2 = this.minPQ.dequeue();
+      this.maxPQ.enqueue(num2);
+      this.minPQ.enqueue(num1);
+    }
+
+    // Balance the sizes
+    if (this.maxPQ.size() > this.minPQ.size() + 1) {
+      this.minPQ.enqueue(this.maxPQ.dequeue());
+    } else if (this.minPQ.size() > this.maxPQ.size()) {
+      this.maxPQ.enqueue(this.minPQ.dequeue());
+    }
+  }
+
+  findMedian() {
+    if (this.maxPQ.size() > this.minPQ.size()) return this.maxPQ.front();
+
+    const mid1 = this.maxPQ.front();
+    const mid2 = this.minPQ.front();
+    return (mid1 + mid2) / 2;
+  }
+}
---
+++ problem-statements/heap/median-finder/median-finder.spec.js
@@ -0,0 +1,143 @@
+import { test, describe, expect } from "vitest";
+import { MedianFinder } from "./median-finder";
+
+describe("median finder", () => {
+  const scenerios = [
+    {
+      name: "example 1",
+      operations: ["addNum", "addNum", "findMedian", "addNum", "findMedian"],
+      inputs: [[1], [2], [], [3], []],
+      expected: [null, null, 1.5, null, 2.0],
+    },
+    {
+      name: "example 2",
+      operations: [
+        "addNum",
+        "addNum",
+        "addNum",
+        "addNum",
+        "addNum",
+        "addNum",
+        "findMedial",
+      ],
+      inputs: [[2], [3], [4], [5], [10], [20], []],
+      expected: [null, null, null, null, null, null, 4.5],
+    },
+    {
+      name: "example 3",
+      operations: [
+        "addNum",
+        "findMedian",
+        "addNum",
+        "findMedian",
+        "addNum",
+        "findMedian",
+        "addNum",
+        "findMedian",
+        "addNum",
+        "findMedian",
+        "addNum",
+        "findMedian",
+        "addNum",
+        "findMedian",
+        "addNum",
+        "findMedian",
+        "addNum",
+        "findMedian",
+        "addNum",
+        "findMedian",
+        "addNum",
+        "findMedian",
+      ],
+      inputs: [
+        [6],
+        [],
+        [10],
+        [],
+        [2],
+        [],
+        [6],
+        [],
+        [5],
+        [],
+        [0],
+        [],
+        [6],
+        [],
+        [3],
+        [],
+        [1],
+        [],
+        [0],
+        [],
+        [0],
+        [],
+      ],
+      expected: [
+        null,
+        6.0,
+        null,
+        8.0,
+        null,
+        6.0,
+        null,
+        6.0,
+        null,
+        6.0,
+        null,
+        5.5,
+        null,
+        6.0,
+        null,
+        5.5,
+        null,
+        5.0,
+        null,
+        4.0,
+        null,
+        3.0,
+      ],
+    },
+  ];
+
+  for (const scenerio of scenerios) {
+    test(scenerio.name, () => {
+      const medianFinder = new MedianFinder();
+      const N = scenerio.operations.length;
+
+      for (let i = 0; i < N; i++) {
+        const operation = scenerio.operations[i];
+        const input = scenerio.inputs[i][0];
+        const expectedVal = scenerio.expected[i];
+
+        if (operation === "addNum") {
+          medianFinder.addNum(input);
+        } else if (operation === "findMedial") {
+          expect(medianFinder.findMedian()).toBe(expectedVal);
+        }
+      }
+    });
+  }
+
+  // test("1", () => {
+  //   const medianFinder = new MedianFinder();
+  //   medianFinder.addNum(2);
+  //   medianFinder.addNum(3);
+  //   medianFinder.addNum(4);
+  //   medianFinder.addNum(5);
+  //   medianFinder.addNum(10);
+  //   medianFinder.addNum(20);
+
+  //   expect(medianFinder.findMedian()).toBe(4.5);
+  // });
+
+  // test("2", () => {
+  //   const medianFinder = new MedianFinder();
+  //   medianFinder.addNum(6);
+  //   expect(medianFinder.findMedian()).toBe(6);
+
+  //   // ["addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian"]
+  //   // [[],[10],[],[2],[],[6],[],[5],[],[0],[],[6],[],[3],[],[1],[],[0],[],[0],[]]
+  //   // [null,8.00000,null,6.00000,null,6.00000,null,6.00000,null,5.50000,null,6.00000,null,5.50000,null,5.00000,null,4.00000,null,3.00000]
+  // });
+});
