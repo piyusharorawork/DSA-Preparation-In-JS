6d68b5fcb9a2208b323a5b5a13ab8d66ea0034b9
piyush arora <piyusharorawork@gmail.com>
2024-09-19 06:20:17 +0530

    drawing sort array

--- problem-statements/recursion/sort-recursive/sort-recursive.js
+++ problem-statements/recursion/sort-recursive/sort-recursive.js
@@ -1,4 +1,4 @@
-export function sortRecursive(nums) {
+export function sortRecursiveV1(nums) {
   const N = nums.length;
   const sort = (n) => {
     // if length of array is 0
@@ -25,4 +25,26 @@ export function sortRecursive(nums) {
   };
 
   return sort(N);
+}
+
+export function sortRecursive(nums) {
+  if (nums.length < 2) return nums;
+  const last = nums.pop();
+  const array = sortRecursive(nums);
+
+  // insert last element to sorted array
+  if (last >= array[array.length - 1]) array.push(last);
+  else if (last <= array[0]) array.unshift(last);
+  else {
+    let left = 0;
+    let right = array.length - 1;
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+      if (array[mid] <= last) left = mid + 1;
+      else right = mid - 1;
+    }
+    array.splice(left, 0, last);
+  }
+
+  return array;
 }
--- syntax-sugars/array.syntax-sugars.js
+++ syntax-sugars/array.syntax-sugars.js
@@ -1,9 +1,13 @@
 const array = [1, 2, 3, 4];
 
 // To remove the element from beg
-array.shift();
+array.shift(); // O(n)
 console.log(array); // [2,3,4]
 
 // To remove the element from end
-array.unshift(100);
+array.unshift(100); // O(n)
 console.log(array); // [100,2,3,4]
+
+// To insert the element at a specific index
+array.splice(3, 0, 31); // O(n)
+console.log(array); // [100,2,3,31,4]
