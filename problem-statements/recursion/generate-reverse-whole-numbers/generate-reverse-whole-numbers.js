133ce9d993431c0933174f6cc0da5f9bcb9d937b
piyush arora <piyusharorawork@gmail.com>
2024-09-19 05:47:01 +0530

    generate whole numbers drawings

--- problem-statements/recursion/generate-reverse-whole-numbers/generate-reverse-whole-numbers.js
+++ problem-statements/recursion/generate-reverse-whole-numbers/generate-reverse-whole-numbers.js
@@ -1,4 +1,13 @@
+// O(N2)
+export function generateReverseWholeNumbersV1(n) {
+  if (n === 1) return [1];
+  const list = generateReverseWholeNumbersV1(n - 1);
+  return [n, ...list];
+}
+
 export function generateReverseWholeNumbers(n) {
   if (n === 1) return [1];
-  return [n, ...generateReverseWholeNumbers(n - 1)];
+  const list = generateReverseWholeNumbers(n - 1);
+  list.unshift(n);
+  return list;
 }
---
+++ syntax-sugars/array.syntax-sugars.js
@@ -0,0 +1,9 @@
+const array = [1, 2, 3, 4];
+
+// To remove the element from beg
+array.shift();
+console.log(array); // [2,3,4]
+
+// To remove the element from end
+array.unshift(100);
+console.log(array); // [100,2,3,4]
