e6a9a8a317439599be559edb4bc505e86f5dbe5f
piyush arora <piyusharorawork@gmail.com>
2024-09-21 20:08:23 +0530

    lca bst

--- problem-statements/trees/bt-from-pre-and-in-order/bt-from-pre-and-in-order.js
+++ problem-statements/trees/bt-from-pre-and-in-order/bt-from-pre-and-in-order.js
@@ -1,6 +1,6 @@
 import { TreeNode } from "../../../data-structures/binary-tree/binary-tree";
 
-export function buildTree(preorder, inorder) {
+export function buildTreeV1(preorder, inorder) {
   const N = preorder.length;
   if (N === 1) return new TreeNode(preorder[0]);
 
@@ -33,4 +33,28 @@ export function buildTree(preorder, inorder) {
   };
 
   return helper();
+}
+
+export function buildTree(preorder, inorder) {
+  const N = preorder.length;
+
+  const inIndexMap = {};
+  inorder.forEach((item, index) => {
+    inIndexMap[item] = index;
+  });
+
+  const createTreeNode = (preIdx, left, right) => {
+    if (preIdx >= N || left > right) return null;
+
+    const node = new TreeNode(preorder[preIdx]);
+    const inIdx = inIndexMap[node.val];
+
+    const leftSubtreeSize = inIdx - left;
+
+    node.left = createTreeNode(preIdx + 1, left, inIdx - 1);
+    node.right = createTreeNode(preIdx + 1 + leftSubtreeSize, inIdx + 1, right);
+    return node;
+  };
+
+  return createTreeNode(0, 0, N - 1);
 }
