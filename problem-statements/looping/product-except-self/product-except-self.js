f1668f78678f445e8dc94cf352935bdc6a79c8b3
piyush arora <piyusharorawork@gmail.com>
2024-09-22 09:16:10 +0530

    merge intervals

--- problem-statements/looping/product-except-self/product-except-self.js
+++ problem-statements/looping/product-except-self/product-except-self.js
@@ -1,4 +1,4 @@
-export function productExceptSelf(nums) {
+export function productExceptSelfV1(nums) {
   const N = nums.length;
 
   const left = new Array(N).fill(1);
@@ -16,6 +16,26 @@ export function productExceptSelf(nums) {
 
   for (let i = 0; i < N; i++) {
     result[i] = left[i] * right[i];
+  }
+
+  return result;
+}
+
+export function productExceptSelf(nums) {
+  const N = nums.length;
+  const result = new Array(N).fill(1); // we will ignore this space
+
+  // First pass
+  for (let i = 1; i < N; i++) {
+    result[i] = result[i - 1] * nums[i - 1];
+  }
+
+  // Now we know after first pass
+  // result[N-1] we have already computed
+  let right = 1;
+  for (let i = N - 1; i >= 0; i--) {
+    result[i] = right * result[i];
+    right *= nums[i];
   }
 
   return result;
